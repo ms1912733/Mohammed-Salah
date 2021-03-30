d443b8bb29871b946cb886cda53160fba1cdb438
Mohammed Salah <81565686+ms1912733@users.noreply.github.com>
2021-03-30 20:13:54 +0300

    Update Citizen.java

--- Citizen.java
+++ Citizen.java
@@ -1,12 +1,30 @@
 
-public class Citizen {
+public class Citizen extends Customer{
 	private int nationalNo;
 	private Date bDate;
 	
-	public Citizen(int nationalNo, Date bDate) {
-		super();
+	public Citizen(int id, String name, int nationalNo, Date bDate) {
+		super(id, name);
 		this.nationalNo = nationalNo;
 		this.bDate = bDate;
 	}
 
-}
+	public int getNationalNo() {
+		return nationalNo;
+	}
+
+	public void setNationalNo(int nationalNo) {
+		this.nationalNo = nationalNo;
+	}
+
+	public Date getbDate() {
+		return bDate;
+	}
+
+	public void setbDate(Date bDate) {
+		this.bDate = bDate;
+	}
+	
+	
+	}
+
